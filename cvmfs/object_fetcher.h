9c96e7069b54e001f5050aa3748e3f73b1585cb9
Siempie30 <siemvermeulen@hotmail.com>
2025-04-10 11:14:15 +0200

    Revert "Removed hash check in fetch reflogs", as the clash this check caused has been properly fixed now, so it can be re-enabled.
    This reverts commit 1ce0fd1145757cfc19c9cd3a51ce995234853a17.

--- cvmfs/object_fetcher.h
+++ cvmfs/object_fetcher.h
@@ -194,7 +194,10 @@ class AbstractObjectFetcher : public ObjectFetcherFailures {
     // Ensure data integrity
     shash::Any computed_hash(reflog_hash.algorithm);
     ReflogTN::HashDatabase(tmp_path, &computed_hash);
-    LogCvmfs(kLogDownload, kLogStderr, "skipping hash check in fetch reflog"); 
+    if (computed_hash != reflog_hash) {
+      unlink(tmp_path.c_str());
+      return kFailBadData;
+    }
 
     *reflog = ReflogTN::Open(tmp_path);
     if (NULL == *reflog) {
