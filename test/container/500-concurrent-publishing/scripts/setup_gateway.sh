d6ee134b3f23315f98d6f2bef56d3df03dc9067f
Siempie30 <siemvermeulen@hotmail.com>
2025-04-15 16:11:31 +0200

    Containerized concurrent testing: - missing dependencies are now installed (apache, usbutils and fuse) - Cvmfs git repository is now manually cloned (instead of taking the volume from the host), because docker volumes are in principle not available during build time - Missing buildpackage dir is now created - Gateway repo.json is now properly created and set

--- test/container/500-concurrent-publishing/scripts/setup_gateway.sh
+++ test/container/500-concurrent-publishing/scripts/setup_gateway.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "plain_text mykey mysecret" > /etc/cvmfs/keys/test.repo.org.gw
+cp /etc/cvmfs/gateway/repo.json.backup /etc/cvmfs/gateway/repo.json
 systemctl start httpd
 cvmfs_server mkfs -o root  test.repo.org
 systemctl start cvmfs-gateway
