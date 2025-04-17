bf41fa0b91a2a7bfc65736bbd275c0467a67a764
Siempie30 <siemvermeulen@hotmail.com>
2025-04-17 10:46:27 +0200

    Container testing: fixed s3 bucket creation and cvmfs s3 config file. Added numbered exits to gateway setup script for easier traceability of what went wrong

--- test/container/500-concurrent-publishing/scripts/setup_gateway.sh
+++ test/container/500-concurrent-publishing/scripts/setup_gateway.sh
@@ -4,9 +4,10 @@
 curl -o /usr/local/bin/mc https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x /usr/local/bin/mc
 
-# Configure bucket
+# Configure and create the bucket
 mc alias set local http://cvmfs-s3:9000 minioadmin minioadmin123
 mc mb local/mybucket
+mc anonymous set public local/mybucket
 
 echo "plain_text mykey mysecret" > /etc/cvmfs/keys/test.repo.org.gw
 cp /etc/cvmfs/gateway/repo.json.backup /etc/cvmfs/gateway/repo.json
@@ -14,10 +15,11 @@ systemctl start httpd
 
 echo "CVMFS_S3_ACCESS_KEY=minioadmin
 CVMFS_S3_SECRET_KEY=minioadmin123
-CVMFS_S3_HOST=localhost:9000
+CVMFS_S3_HOST=cvmfs-s3
+CVMFS_S3_PORT=9000
 CVMFS_S3_BUCKET=mybucket
 CVMFS_S3_DNS_BUCKETS=false
 CVMFS_S3_USE_HTTPS=false" > /etc/cvmfs/s3.conf
 
-cvmfs_server mkfs -s /etc/cvmfs/s3.conf -w http://localhost:9000/mybucket -o root  test.repo.org
+cvmfs_server mkfs -s /etc/cvmfs/s3.conf -w http://cvmfs-s3:9000/mybucket -o root  test.repo.org
 systemctl start cvmfs-gateway
--- test/container/500-concurrent-publishing/setup.sh
+++ test/container/500-concurrent-publishing/setup.sh
@@ -1,4 +1,4 @@
-docker exec -it cvmfs-gw1 /scripts/setup_gateway.sh || exit $?
-docker exec -it cvmfs-pub1 /scripts/setup_connected_publisher.sh || exit $? 
-docker exec -it cvmfs-pub2 /scripts/setup_connected_publisher.sh || exit $?
+docker exec -it cvmfs-gw1 /scripts/setup_gateway.sh || exit 1
+docker exec -it cvmfs-pub1 /scripts/setup_connected_publisher.sh || exit 2
+docker exec -it cvmfs-pub2 /scripts/setup_connected_publisher.sh || exit 3
 
