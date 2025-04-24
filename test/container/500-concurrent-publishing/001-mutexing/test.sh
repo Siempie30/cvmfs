f90b6a570b06597a6d25d78e34fcf28be5487003
Siempie30 <siemvermeulen@hotmail.com>
2025-04-24 17:09:02 +0200

    Added util function for executing functions in a container, while maintaining the return code of the executed function

--- test/container/500-concurrent-publishing/001-mutexing/test.sh
+++ test/container/500-concurrent-publishing/001-mutexing/test.sh
@@ -1,21 +1,25 @@
-docker exec  cvmfs-pub1 curl -X POST --data "{\"repo\":\"test.repo.org\"}" http://cvmfs-gw1:4929/api/v1/token-ring
+UTIL_SCRIPT="$(dirname $0)/../test_util.sh"
+. $UTIL_SCRIPT
+
+execute_in_container cvmfs-pub1 "curl -X POST --data '{\"repo\":\"test.repo.org\"}' http://cvmfs-gw1:4929/api/v1/token-ring" || exit 1
 
 echo "\n\n---Transaction 1: pub 1 (to gw1)"
-docker exec  cvmfs-pub1 cvmfs_server transaction
-docker exec  cvmfs-pub1 bash -c 'echo abc > /cvmfs/test.repo.org/testfile'
+execute_in_container cvmfs-pub1 "cvmfs_server transaction" || exit 2
+execute_in_container cvmfs-pub1 "bash -c 'echo abc > /cvmfs/test.repo.org/testfile'" || exit 3
 
 echo "\n\n---Publishing transaction 1"
-docker exec  cvmfs-pub1 cvmfs_server publish
+execute_in_container cvmfs-pub1 "cvmfs_server publish" || exit 4
 
 sleep 5
 
 echo "\n\n---Transaction 2: pub 2 (to gw2)"
-docker exec  cvmfs-pub2 cvmfs_server transaction
-docker exec  cvmfs-pub2 bash -c 'echo def > /cvmfs/test.repo.org/testfile'
+execute_in_container cvmfs-pub2 "cvmfs_server transaction" || exit 5
+execute_in_container cvmfs-pub2 "bash -c 'echo def > /cvmfs/test.repo.org/testfile'" || exit 6
 
 echo "\n\n---Publishing transaction 2"
-docker exec  cvmfs-pub2 cvmfs_server publish
+execute_in_container cvmfs-pub2 "cvmfs_server publish" || exit 7
 
-docker exec  cvmfs-gw1 cvmfs_server tag -l
-docker exec  cvmfs-gw1 cat /cvmfs/test.repo.org/testfile
+execute_in_container cvmfs-gw1 "cvmfs_server mount; cat /cvmfs/test.repo.org/testfile" || exit 8
+docker exec  cvmfs-gw1 cat /cvmfs/test.repo.org/testfile | tee | grep def ||  exit 9
 
+exit 0
