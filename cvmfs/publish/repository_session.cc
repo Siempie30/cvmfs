27b22e159e4ef6f05e2aa7b63586755cd9710b86
Siempie30 <siemvermeulen@hotmail.com>
2025-05-12 16:34:08 +0200

    Fixed typo in SQL query (missing space)

--- cvmfs/publish/repository_session.cc
+++ cvmfs/publish/repository_session.cc
@@ -468,7 +468,7 @@ bool Publisher::Session::UpdateGatewayDb(const std::string& repo_path, const std
                EPublish::kFailSqlite);
       }
       // Set a new default gateway: the first gateway with status 0
-      std::string set_default_query = "UPDATE gateway SET default_gw = 1 WHERE rowid = (SELECT rowid FROM gateway WHERE status = " + std::to_string(kGatewayUp) + "LIMIT 1);";
+      std::string set_default_query = "UPDATE gateway SET default_gw = 1 WHERE rowid = (SELECT rowid FROM gateway WHERE status = " + std::to_string(kGatewayUp) + " LIMIT 1);";
       rc = sqlite3_exec(db, set_default_query.c_str(), NULL, NULL, &errmsg);
       if (rc != SQLITE_OK) {
       sqlite3_exec(db, "ROLLBACK;", NULL, NULL, NULL);
@@ -521,7 +521,7 @@ void Publisher::Session::GetRingGwsByPriority(const std::string &repo_path, std:
                    EPublish::kFailSqlite);
   }
 
-  std::string query = "SELECT address FROM gateway WHERE status <= " + std::to_string(kGatewayHighLoad) + "ORDER BY default_gw DESC, status ASC;";
+  std::string query = "SELECT address FROM gateway WHERE status <= " + std::to_string(kGatewayHighLoad) + " ORDER BY default_gw DESC, status ASC;";
   LogCvmfs(kLogPublish, kLogStderr, "Session acquire: query: %s", query.c_str());
   sqlite3_stmt* stmt = NULL;
 
