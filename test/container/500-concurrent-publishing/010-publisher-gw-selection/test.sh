8982fb42a904004020353edd07faa8abbc68b950
Siempie30 <siemvermeulen@hotmail.com>
2025-05-13 15:40:29 +0200

    Reverted lease time change in test 10, finished test 10

--- test/container/500-concurrent-publishing/010-publisher-gw-selection/test.sh
+++ test/container/500-concurrent-publishing/010-publisher-gw-selection/test.sh
@@ -15,3 +15,17 @@ echo "\n---Verifying succesful lease on gateway 2"
 OUTPUT=$(docker exec -it cvmfs-gw2 curl -X GET http://cvmfs-gw2:4929/api/v1/leases | jq '.data."test.repo.org/".hostname')
 echo "$OUTPUT"
 if [ "$OUTPUT" != "\"cvmfs-pub1\"" ]; then exit 4; fi
+
+echo "\n---Writing changes"
+execute_in_container cvmfs-pub1 "echo 'abc' > /cvmfs/test.repo.org/someFile" || exit 5
+
+echo "\n---Publishing changes"
+execute_in_container cvmfs-pub1 "cvmfs_server publish" || exit 6
+# NOTE: This test currently fails because the publish command makes use of the upstream storage value configured in server.conf
+# This value is not yet updated when the default gateway (and therefore the gateway on which the lease is acquired) changes.
+# Should the server.conf file be updated when the default gateway changes, or should the server.conf file read the token ring db
+# to modify the upstream storage value passed to swissknife sync?
+
+echo "\n---Verifying succesful changes on gateway 2"
+execute_in_container cvmfs-gw2 "cvmfs_server mount test.repo.org" || exit 7
+execute_in_container cvmfs-gw2 "cat /cvmfs/test.repo.org/someFile | tee | grep \"abc\"" || exit 8
