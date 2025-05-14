baea516b9e2629dd0d65528b1d924172bc8599b3
Siempie30 <siemvermeulen@hotmail.com>
2025-05-14 10:30:17 +0200

    Added test to verify gateway cannot doubly accept token for a repo.

---
+++ test/container/500-concurrent-publishing/011-gw-token-accept-check/test.sh
@@ -0,0 +1,23 @@
+SCRIPT_DIR=$(dirname $0)
+UTIL_SCRIPT="$SCRIPT_DIR/../test_util.sh"
+. $UTIL_SCRIPT
+
+echo "\n---Handing out token to gateway 1"
+execute_in_container cvmfs-gw1 "curl -s -X POST --data '{\"repo\":\"test.repo.org\"}' http://cvmfs-gw1:4929/api/v1/token-ring" || exit 1
+
+echo "\n---Verifying gateway 1 has token"
+has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "true" ]; then exit 2; fi
+
+sleep 2
+
+echo "\n---Handing out token to gateway 1 (again)"
+response=$(docker exec -it cvmfs-gw1 curl -s -X POST --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.acknowledgement')
+if [ "$response" != "\"error\"" ]; then exit 3; fi
+
+sleep 1
+
+echo "\n---Verifying gateway 1 no longer has token"
+has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "false" ]; then exit 4; fi
+
