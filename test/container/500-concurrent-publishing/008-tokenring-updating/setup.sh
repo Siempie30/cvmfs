b2156dfc5c5b7e3803080efa840cb46e3fcc1acb
Siempie30 <siemvermeulen@hotmail.com>
2025-05-23 17:38:54 +0200

    Updated test 8 to verify token ring sorting order.

--- test/container/500-concurrent-publishing/008-tokenring-updating/setup.sh
+++ test/container/500-concurrent-publishing/008-tokenring-updating/setup.sh
@@ -7,8 +7,8 @@ cp $SCRIPT_DIR/repo.json $SCRIPT_DIR/../config/gw1/repo.json
 docker exec -it cvmfs-gw1 /scripts/gateway_mkfs.sh $REPO_NAME || exit 1
 docker exec -it cvmfs-gw1 systemctl start cvmfs-gateway
 
-cp $SCRIPT_DIR/user.json $SCRIPT_DIR/../config/gw2/user.json
-cp $SCRIPT_DIR/repo.json $SCRIPT_DIR/../config/gw2/repo.json
-docker exec -it cvmfs-gw2 /scripts/gateway_mkfs.sh -E $REPO_NAME || exit 2
-docker exec -it cvmfs-gw2 systemctl start cvmfs-gateway
+cp $SCRIPT_DIR/user.json $SCRIPT_DIR/../config/gw3/user.json
+cp $SCRIPT_DIR/repo.json $SCRIPT_DIR/../config/gw3/repo.json
+docker exec -it cvmfs-gw3 /scripts/gateway_mkfs.sh -E $REPO_NAME || exit 2
+docker exec -it cvmfs-gw3 systemctl start cvmfs-gateway
 
--- test/container/500-concurrent-publishing/008-tokenring-updating/test.sh
+++ test/container/500-concurrent-publishing/008-tokenring-updating/test.sh
@@ -5,25 +5,25 @@ DB_FILE="/var/lib/cvmfs-gateway/gw.db"
 
 echo "---Verifying initial token ring tables"
 EXPECTED="http://cvmfs-gw1:4929/api/v1|test.repo.org|0
-http://cvmfs-gw2:4929/api/v1|test.repo.org|0"
+http://cvmfs-gw3:4929/api/v1|test.repo.org|0"
 OUTPUT=$(execute_in_container cvmfs-gw1 "sqlite3 $DB_FILE \"SELECT * FROM TokenRing;\"")
 echo "$OUTPUT"
 if [ "$OUTPUT" != "$EXPECTED" ]; then exit 1; fi
-OUTPUT=$(execute_in_container cvmfs-gw2 "sqlite3 $DB_FILE \"SELECT * FROM TokenRing;\"")
+OUTPUT=$(execute_in_container cvmfs-gw3 "sqlite3 $DB_FILE \"SELECT * FROM TokenRing;\"")
 echo "$OUTPUT"
 if [ "$OUTPUT" != "$EXPECTED" ]; then exit 2; fi
 
-echo "\n---Creating third gateway"
-cp $SCRIPT_DIR/user.json $SCRIPT_DIR/../config/gw3/user.json
-cp $SCRIPT_DIR/token_ring.json $SCRIPT_DIR/../config/gw3/token_ring.json
-cp $SCRIPT_DIR/repo.json $SCRIPT_DIR/../config/gw3/repo.json
-docker exec -it cvmfs-gw3 /scripts/gateway_mkfs.sh -E test.repo.org || exit 3
-docker exec -it cvmfs-gw3 systemctl start cvmfs-gateway
+echo "\n---Creating gateway 2"
+cp $SCRIPT_DIR/user.json $SCRIPT_DIR/../config/gw2/user.json
+cp $SCRIPT_DIR/token_ring.json $SCRIPT_DIR/../config/gw2/token_ring.json
+cp $SCRIPT_DIR/repo.json $SCRIPT_DIR/../config/gw2/repo.json
+docker exec -it cvmfs-gw2 /scripts/gateway_mkfs.sh -E test.repo.org || exit 3
+docker exec -it cvmfs-gw2 systemctl start cvmfs-gateway
 
 echo "\n---Verifying updated token ring tables"
 EXPECTED="http://cvmfs-gw1:4929/api/v1|test.repo.org|0
-http://cvmfs-gw2:4929/api/v1|test.repo.org|0
-http://cvmfs-gw3:4929/api/v1|test.repo.org|0"
+http://cvmfs-gw3:4929/api/v1|test.repo.org|0
+http://cvmfs-gw2:4929/api/v1|test.repo.org|0"
 OUTPUT=$(execute_in_container cvmfs-gw1 "sqlite3 $DB_FILE \"SELECT * FROM TokenRing;\"")
 echo "$OUTPUT"
 if [ "$OUTPUT" != "$EXPECTED" ]; then exit 4; fi
@@ -34,54 +34,82 @@ OUTPUT=$(execute_in_container cvmfs-gw3 "sqlite3 $DB_FILE \"SELECT * FROM TokenR
 echo "$OUTPUT"
 if [ "$OUTPUT" != "$EXPECTED" ]; then exit 6; fi
 
-echo "\n---Passing token to gateway 1"
-execute_in_container cvmfs-gw1 "curl -s -X POST --data '{\"repo\":\"test.repo.org\"}' http://cvmfs-gw1:4929/api/v1/token-ring" || exit 7
+EXPECTED="{
+  \"address\": \"http://cvmfs-gw1:4929/api/v1\",
+  \"status\": 0
+}
+{
+  \"address\": \"http://cvmfs-gw2:4929/api/v1\",
+  \"status\": 0
+}
+{
+  \"address\": \"http://cvmfs-gw3:4929/api/v1\",
+  \"status\": 0
+}"
+OUTPUT=$(execute_in_container cvmfs-gw1 "curl -s -X GET --data '{\"repo\":\"test.repo.org\"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.gateways[]'")
+echo "$OUTPUT"
+if [ "$OUTPUT" != "$EXPECTED" ]; then exit 7; fi
+OUTPUT=$(execute_in_container cvmfs-gw2 "curl -s -X GET --data '{\"repo\":\"test.repo.org\"}' http://cvmfs-gw2:4929/api/v1/token-ring | jq '.gateways[]'")
+echo "$OUTPUT"
+if [ "$OUTPUT" != "$EXPECTED" ]; then exit 8; fi
+OUTPUT=$(execute_in_container cvmfs-gw3 "curl -s -X GET --data '{\"repo\":\"test.repo.org\"}' http://cvmfs-gw3:4929/api/v1/token-ring | jq '.gateways[]'")
+echo "$OUTPUT"
+if [ "$OUTPUT" != "$EXPECTED" ]; then exit 9; fi
 
-echo "\n---Removing gateway 2"
-execute_in_container cvmfs-gw2 "systemctl stop cvmfs-gateway" || exit 8
+echo "\n---Passing token to gateway 1"
+execute_in_container cvmfs-gw1 "curl -s -X POST --data '{\"repo\":\"test.repo.org\"}' http://cvmfs-gw1:4929/api/v1/token-ring" || exit 10
 
 sleep 3
 
-echo "\n---Verifying gateway 3 has token"
-has_token=$(docker exec -it cvmfs-gw3 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw3:4929/api/v1/token-ring | jq '.has_token')
-if [ "$has_token" != "true" ]; then exit 9; fi
+echo "\n---Verifying gateway 2 has token"
+has_token=$(docker exec -it cvmfs-gw2 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw2:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "true" ]; then exit 11; fi
+
+echo "\n---Removing gateway 3"
+execute_in_container cvmfs-gw3 "systemctl stop cvmfs-gateway" || exit 12
+
+sleep 3
+
+echo "\n---Verifying gateway 1 has token"
+has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "true" ]; then exit 13; fi
 
 echo "\n---Verifying updated token ring tables"
 EXPECTED="http://cvmfs-gw1:4929/api/v1|test.repo.org|0
-http://cvmfs-gw2:4929/api/v1|test.repo.org|3
-http://cvmfs-gw3:4929/api/v1|test.repo.org|0"
+http://cvmfs-gw3:4929/api/v1|test.repo.org|3
+http://cvmfs-gw2:4929/api/v1|test.repo.org|0"
 OUTPUT=$(execute_in_container cvmfs-gw1 "sqlite3 $DB_FILE \"SELECT * FROM TokenRing;\"")
 echo "$OUTPUT"
-if [ "$OUTPUT" != "$EXPECTED" ]; then exit 10; fi
-OUTPUT=$(execute_in_container cvmfs-gw3 "sqlite3 $DB_FILE \"SELECT * FROM TokenRing;\"")
+if [ "$OUTPUT" != "$EXPECTED" ]; then exit 14; fi
+OUTPUT=$(execute_in_container cvmfs-gw2 "sqlite3 $DB_FILE \"SELECT * FROM TokenRing;\"")
 echo "$OUTPUT"
-if [ "$OUTPUT" != "$EXPECTED" ]; then exit 11; fi
+if [ "$OUTPUT" != "$EXPECTED" ]; then exit 15; fi
 
 sleep 3
 
-echo "\n---Verifying gateway 1 has token and gateway 3 doesn't"
+echo "\n---Verifying gateway 2 has token and gateway 1 doesn't"
+has_token=$(docker exec -it cvmfs-gw2 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw2:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "true" ]; then exit 16; fi
 has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
-if [ "$has_token" != "true" ]; then exit 12; fi
-has_token=$(docker exec -it cvmfs-gw3 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw3:4929/api/v1/token-ring | jq '.has_token')
-if [ "$has_token" != "false" ]; then exit 13; fi
+if [ "$has_token" != "false" ]; then exit 17; fi
 
-echo "\n---Stopping gateway 1"
-execute_in_container cvmfs-gw1 "systemctl stop cvmfs-gateway" || exit 13
+echo "\n---Stopping gateway 2"
+execute_in_container cvmfs-gw2 "systemctl stop cvmfs-gateway" || exit 18
 
-echo "\n---Verifying gateway 3 has token during cycle time (16 seconds)"
+echo "\n---Verifying gateway 1 has token during cycle time (16 seconds)"
 
 sleep 4
-has_token=$(docker exec -it cvmfs-gw3 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw3:4929/api/v1/token-ring | jq '.has_token')
-if [ "$has_token" != "false" ]; then exit 14; fi
+has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "false" ]; then exit 18; fi
 
 sleep 4
-has_token=$(docker exec -it cvmfs-gw3 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw3:4929/api/v1/token-ring | jq '.has_token')
-if [ "$has_token" != "false" ]; then exit 15; fi
+has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "false" ]; then exit 19; fi
 
 sleep 4
-has_token=$(docker exec -it cvmfs-gw3 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw3:4929/api/v1/token-ring | jq '.has_token')
-if [ "$has_token" != "false" ]; then exit 16; fi
+has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "false" ]; then exit 20; fi
 
 sleep 4
-has_token=$(docker exec -it cvmfs-gw3 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw3:4929/api/v1/token-ring | jq '.has_token')
-if [ "$has_token" != "true" ]; then exit 17; fi
+has_token=$(docker exec -it cvmfs-gw1 curl -s -X GET --data '{"repo":"test.repo.org"}' http://cvmfs-gw1:4929/api/v1/token-ring | jq '.has_token')
+if [ "$has_token" != "true" ]; then exit 21; fi
